08a46f635944fb3be770fc1746f801d05a467efb
Derek Kastner <dkastner@gmail.com>
2010-11-12 14:29:19 -0500

    Fixes to allow specs to run within rake task

--- spec/emitter/tasks_spec.rb
+++ spec/emitter/tasks_spec.rb
@@ -3,6 +3,13 @@ require 'sandbox'
 require 'emitter/tasks'
 
 describe Emitter::Tasks do
+  def git(cmd, dir = nil, &blk)
+    full_cmd = ''
+    full_cmd << "cd #{dir} && " if dir
+    full_cmd << "unset GIT_DIR && unset GIT_INDEX_FILE && unset GIT_WORK_TREE && git #{cmd}"
+    `#{full_cmd}`
+  end
+
   let(:tasks) { Emitter::Tasks.new }
   let(:emitter_path) { File.expand_path '../../', File.dirname(__FILE__) }
   let(:rakefile) do
@@ -18,7 +25,7 @@ Emitter::Tasks.define
       it 'should generate docs' do
         Sandbox.play do |path|
           flight_path = File.join(path, 'flight')
-          `git clone git://github.com/brighterplanet/flight.git #{flight_path}`
+          git "clone git://github.com/brighterplanet/flight.git #{flight_path}"
           File.open(File.join(flight_path, 'Rakefile'), 'w') do |f|
             f.puts rakefile
           end
@@ -34,7 +41,7 @@ Emitter::Tasks.define
       it 'should commit any changed doc files' do
         Sandbox.play do |path|
           flight_path = File.join(path, 'flight')
-          `git clone git://github.com/brighterplanet/flight.git #{flight_path}`
+          git "clone git://github.com/brighterplanet/flight.git #{flight_path}"
           File.open(File.join(flight_path, 'Rakefile'), 'w') do |f|
             f.puts rakefile
           end
@@ -42,7 +49,8 @@ Emitter::Tasks.define
           `cd #{flight_path} && mkdir docs && echo "<html>" > docs/foobar.html`
           `cd #{flight_path} && NO_PUSH=true rake pages`
 
-          `cd #{flight_path}/docs && unset GIT_DIR && unset GIT_INDEX_FILE && unset GIT_WORK_TREE && git log -n 1`.should =~ /rebuild pages/
+          git('log -n 1', File.join(flight_path, 'docs')).
+            should =~ /rebuild pages/
         end
       end
     end
